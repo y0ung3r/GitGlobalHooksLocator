022c7fe66b5e6d0c74590f70595fde5b47ed3441
Никита Великий <mtyoungstz@outlook.com>
2023-11-28 00:31:37 +0500

    (#7) Replace SemanticVersion with the more specialized GitVersion

--- src/main/kotlin/com/github/y0ung3r/gitglobalhookslocator/git/exceptions/ProvidedSemanticVersionIsInvalidException.kt
+++
@@ -1,4 +0,0 @@
-package com.github.y0ung3r.gitglobalhookslocator.git.exceptions
-
-class ProvidedSemanticVersionIsInvalidException(version: String)
-    : Exception("The provided version does not conform to semantic versioning: $version")
